34fcaae545af09eef3df235b89d5ab055d1c253b
RhazzXIX <jm21tonio@gmail.com>
2023-04-10 16:57:42 +0800

    Install dependencies and add images

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app'
+
+const fbAPP = initializeApp({
+  apiKey: "AIzaSyD3iQ-Ymc_2B5SAPyYQdoOHKc12UH3p5tQ",
+  authDomain: "photo-tagging-app-ee203.firebaseapp.com",
+  projectId: "photo-tagging-app-ee203",
+  storageBucket: "photo-tagging-app-ee203.appspot.com",
+  messagingSenderId: "387871215062",
+  appId: "1:387871215062:web:483ebb3230a6dbaa126877",
+  measurementId: "G-JSFBSH54XH"
+}) 
+
+export default fbAPP
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +22,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
