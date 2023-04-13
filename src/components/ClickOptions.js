7722bd3a8dfc182d3f2907148e3d16df13229434
RhazzXIX <jm21tonio@gmail.com>
2023-04-13 12:36:02 +0800

    Add logic to not show buttons of a found character

--- src/components/ClickOptions.js
+++ src/components/ClickOptions.js
@@ -2,17 +2,16 @@ const ClickOptions = (props) => {
   const { selections } = props;
   return (
     <ul>
-      {selections.map( character => {
-        return(
+      {selections.map((character) => {
+        if (character.isFound) return null;
+        return (
           <li key={character.id}>
-            <button onClick={character.fn}>
-              {character.name}
-            </button>
+            <button onClick={() => character.fn()}>{character.name}</button>
           </li>
-        )
+        );
       })}
     </ul>
   );
 };
 
-export default ClickOptions
+export default ClickOptions;
--- src/tests/ClickOptions.test.js
+++ src/tests/ClickOptions.test.js
@@ -3,12 +3,16 @@ import "@testing-library/jest-dom";
 import { screen, render } from "@testing-library/react";
 import ClickOptions from "../components/ClickOptions";
 
-const selections = [
-  {
-    name: "Luffy",
-    fn: jest.fn(),
-    id: 1,
+const luffy = {
+  name: "Luffy",
+  fn(e) {
+    this.isFound = true;
   },
+  id: 1,
+  isFound: false,
+};
+
+const selections = [
   {
     name: "Heli",
     fn: jest.fn(),
@@ -24,6 +28,11 @@ const selections = [
 const user = userEvent.setup();
 
 describe("ClickOptions component", () => {
+  beforeEach(() => {
+    luffy.isFound = false;
+    selections.push(luffy);
+  });
+
   it("Renders on screen", () => {
     const { container } = render(<ClickOptions selections={selections} />);
     expect(container).toMatchSnapshot();
@@ -33,15 +42,33 @@ describe("ClickOptions component", () => {
     render(<ClickOptions selections={selections} />);
     const buttons = screen.getAllByRole("button");
 
-    await user.click(buttons[0]);
-
-    expect(selections[0].fn).toHaveBeenCalled();
-    expect(selections[1].fn).not.toHaveBeenCalled();
-
-    await user.click(buttons[1]);
     await user.click(buttons[2]);
 
+    expect(selections[2].isFound).toBeTruthy();
+    expect(selections[1].fn).not.toHaveBeenCalled();
+
+    await user.click(buttons[0]);
+    await user.click(buttons[1]);
+
+    expect(selections[0].fn).toHaveBeenCalled();
     expect(selections[1].fn).toHaveBeenCalled();
-    expect(selections[2].fn).toHaveBeenCalled();
   });
+
+  it("Doesn't render the button of a found character", async () => {
+    const { rerender } = render(<ClickOptions selections={selections} />);
+    const luffyBtn = screen.getByRole("button", { name: "Luffy" });
+    let buttons = screen.getAllByRole("button");
+
+    expect(buttons).toHaveLength(3);
+
+    await user.click(luffyBtn);
+    rerender(<ClickOptions selections={selections} />);
+    buttons = screen.getAllByRole("button");
+
+    expect(
+      screen.queryByRole("button", { name: "Luffy" })
+    ).not.toBeInTheDocument();
+    expect(buttons).toHaveLength(2);
+  });
+  afterEach(() => selections.pop());
 });
