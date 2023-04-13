ae152acd1f00c262290a50c4becb4c46f1dad23b
RhazzXIX <jm21tonio@gmail.com>
2023-04-13 08:56:50 +0800

    Replace img element with video element
    Replaced the element due to the decision of showing a video instead of a
    gif file.

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom";
 
-export default function FrontPage (props) {
-  const { img } = props
-  return(
+export default function FrontPage(props) {
+  return (
     <main id="FrontPage">
       <h1>Seek & Click</h1>
       <button>Play!</button>
-      <img src={img.src} alt={img.name} />
-        <p>
-          -----&gt; &nbsp;
-      <Link to='kids'>
-        Kids ver.
-      </Link>
-        </p>
+      <video width={"360px"} autoPlay loop muted>
+        <source src={""} type="video/webm" />
+      </video>
+      <p>
+        -----&gt; &nbsp;
+        <Link to="kids">Kids ver.</Link>
+      </p>
     </main>
-  )
+  );
 }
--- src/tests/FrontPage.test.js
+++ src/tests/FrontPage.test.js
@@ -1,16 +1,16 @@
-import '@testing-library/jest-dom'
-import { screen, render } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import { BrowserRouter } from 'react-router-dom'
-import FrontPage from '../components/FrontPage'
+import "@testing-library/jest-dom";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
+import FrontPage from "../components/FrontPage";
 
-describe('FrontPage component' , () => {
-  it('Renders on screen', () => {
+describe("FrontPage component", () => {
+  it("Renders on screen", () => {
     const { container } = render(
-    <BrowserRouter>
-      <FrontPage img={{}}/>
-    </BrowserRouter>
-    )
-    expect(container).toMatchSnapshot()
-  })
-})
+      <BrowserRouter>
+        <FrontPage />
+      </BrowserRouter>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
