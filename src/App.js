7635f358eb02736d6efd4f084ef6f9249313c634
RhazzXIX <jm21tonio@gmail.com>
2023-04-14 15:59:26 +0800

    Refactor App from component to a RouterProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import FrontPage from "./components/FrontPage";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "photo-tagging-app",
+    element: <Home />,
+    children: [
+      {
+        path: "",
+        element: <FrontPage key={"normal"} />,
+      },
+      {
+        path: "kids",
+        element: <FrontPage key={"kids"} version={"kids"} />,
+      },
+    ],
+  },
+]);
+
+const App = <RouterProvider router={router} />;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { initializeApp } from 'firebase/app'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const fbAPP = initializeApp({
-  apiKey: "AIzaSyD3iQ-Ymc_2B5SAPyYQdoOHKc12UH3p5tQ",
-  authDomain: "photo-tagging-app-ee203.firebaseapp.com",
-  projectId: "photo-tagging-app-ee203",
-  storageBucket: "photo-tagging-app-ee203.appspot.com",
-  messagingSenderId: "387871215062",
-  appId: "1:387871215062:web:483ebb3230a6dbaa126877",
-  measurementId: "G-JSFBSH54XH"
-}) 
-
-export default fbAPP
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode>{App}</React.StrictMode>);
