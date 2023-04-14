2d3d1d01b92d509830d9aaf4179e0b0756fb8919
RhazzXIX <jm21tonio@gmail.com>
2023-04-14 18:16:11 +0800

    Add logic to get & show game images from firebase

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,31 @@
-export default function Game() {
+import { useState, useEffect } from "react";
+import { database } from "../assists/fbConfig";
+import { getDoc, doc, collection } from "firebase/firestore";
+
+export default function Game(props) {
+  const [firstMap, setFirstmap] = useState({});
+  const [scndMap, setScndMap] = useState({});
+  const [showFirstMap, setShowFirstMap] = useState(true);
+  const [showScndMap, setShowScndmap] = useState(true);
+
+  const selction1Ref = doc(database, "game", "anime");
+  const selection2Ref = doc(database, "game", "game");
+
+  const getData = async () => {
+    const data1 = await getDoc(selction1Ref).then((response) =>
+      response.data()
+    );
+    const data2 = await getDoc(selection2Ref).then((response) =>
+      response.data()
+    );
+    setFirstmap(data1.map);
+    setScndMap(data2.map);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <main>
       <header>
@@ -14,9 +41,16 @@ export default function Game() {
           </li>
         </ul>
       </header>
-      <section>
-        <img src="" alt="" />
-      </section>
+      {showFirstMap && (
+        <section>
+          <img src={firstMap.url} alt={firstMap.name} />
+        </section>
+      )}
+      {showScndMap && (
+        <section>
+          <img src={scndMap.url} alt={scndMap.name} />
+        </section>
+      )}
     </main>
   );
 }
