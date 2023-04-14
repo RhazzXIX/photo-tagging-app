a91b3ac4887a8275d68aaa69cc7ed3c3917a311d
RhazzXIX <jm21tonio@gmail.com>
2023-04-14 15:04:36 +0800

    Add logic to get video source from firebase

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,12 +1,25 @@
 import { Link } from "react-router-dom";
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import { storage } from "../assists/fbConfig";
+import { useEffect, useRef, useState } from "react";
 
 export default function FrontPage(props) {
+  const [vidSrc, setVidSrc] = useState();
+  let storageVidLoc = "gameMap/chopper-hiding.webm";
+
+  useEffect(() => {
+    const vidRef = ref(storage, storageVidLoc);
+    getDownloadURL(vidRef).then((url) => {
+      setVidSrc(url);
+    });
+  }, []);
+
   return (
     <main id="FrontPage">
       <h1>Seek & Click</h1>
       <button>Play!</button>
-      <video width={"360px"} autoPlay loop muted>
-        <source src={""} type="video/webm" />
+      <video width={"360px"} key={vidSrc} autoPlay loop muted>
+        <source src={vidSrc} type="video/webm" />
       </video>
       <p>
         -----&gt; &nbsp;
