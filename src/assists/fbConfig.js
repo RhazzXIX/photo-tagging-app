118625fd909c781ba91a3b39f52b92599f5be898
RhazzXIX <jm21tonio@gmail.com>
2023-04-14 14:59:52 +0800

    Put firebase setup on a different js file

---
+++ src/assists/fbConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const fbAPP = initializeApp({
+  apiKey: "AIzaSyD3iQ-Ymc_2B5SAPyYQdoOHKc12UH3p5tQ",
+  authDomain: "photo-tagging-app-ee203.firebaseapp.com",
+  projectId: "photo-tagging-app-ee203",
+  storageBucket: "photo-tagging-app-ee203.appspot.com",
+  messagingSenderId: "387871215062",
+  appId: "1:387871215062:web:483ebb3230a6dbaa126877",
+  measurementId: "G-JSFBSH54XH",
+});
+
+
+const storage = getStorage(fbAPP);
+
+export { storage };
