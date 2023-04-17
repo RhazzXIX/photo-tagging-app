0d3d40583372653702585fe8d9735c65366e5343
RhazzXIX <jm21tonio@gmail.com>
2023-04-17 08:52:54 +0800

    Refactor html structure of Selection component
    Refactored the html structure for integration of styling and media
    sources.

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,37 +1,38 @@
 import useGameData from "../assists/useGameData";
+import "../styles/Selection.css";
 
 export default function Selection(props) {
   const { firstMap, scndMap, charSelection1, charSelection2 } = useGameData();
 
   return (
-    <section>
-      <div>
+    <section id="selection">
+      <div className="card">
         <ul>
           {charSelection1.map((char) => {
             return (
               <li key={char.name}>
-                <img src={char.url} alt={char.name} />
+                <img src={char.url} alt={char.name} className="chars" />
                 <p>{char.name}</p>
               </li>
             );
           })}
         </ul>
-        <img src={firstMap.url} alt={firstMap.name} />
-        <p>{firstMap.name}</p>
+        <img src={firstMap.miniUrl} alt={firstMap.name} />
+        <button>{firstMap.name}</button>
       </div>
-      <div>
+      <div className="card">
         <ul>
           {charSelection2.map((char) => {
             return (
               <li key={char.name}>
-                <img src={char.url} alt={char.name} />
+                <img src={char.url} alt={char.name} className="chars" />
                 <p>{char.name}</p>
               </li>
             );
           })}
         </ul>
-        <img src={scndMap.url} alt={scndMap.name} />
-        <p>{scndMap.name}</p>
+        <img src={scndMap.miniUrl} alt={scndMap.name} />
+        <button>{scndMap.name}</button>
       </div>
     </section>
   );
