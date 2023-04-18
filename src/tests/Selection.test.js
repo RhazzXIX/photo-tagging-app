e852032d90c7ca82e5a56a5019efd6d5d138e2b9
RhazzXIX <jm21tonio@gmail.com>
2023-04-18 16:45:38 +0800

    Mock the useGameData fn to simulate fetching data

--- src/tests/Selection.test.js
+++ src/tests/Selection.test.js
@@ -1,16 +1,56 @@
 import "@testing-library/jest-dom";
-import { screen, render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { BrowserRouter } from "react-router-dom";
+import useGameData from "../assists/useGameData";
+import { render } from "@testing-library/react";
 import Selection from "../components/Selection";
+
+const fetchedData = {
+  firstMap: {
+    miniUrl: "mini url 1",
+    name: "Anime Crossover",
+    url: "url 2",
+  },
+  scndMap: {
+    miniUrl: "mini url 2",
+    name: "Game Crossover",
+    url: "url 2",
+  },
+  charSelection1: [
+    {
+      name: 'Yato "Yaboku"',
+      url: "yato url",
+    },
+    {
+      name: 'ELUCIA"',
+      url: "elucia url",
+    },
+    {
+      name: 'EVANS"',
+      url: "evans url",
+    },
+  ],
+  charSelection2: [
+    {
+      name: "Spyro",
+      url: "spyro url",
+    },
+    {
+      name: 'Arthur"',
+      url: "arthur url",
+    },
+    {
+      name: 'Marco"',
+      url: "marco url",
+    },
+  ],
+};
+
+jest.mock("../assists/useGameData", () => {
+  return () => fetchedData;
+});
 
 describe("Selection component", () => {
   it("Renders on screen", () => {
-    const { container } = render(
-      <BrowserRouter>
-        <Selection />
-      </BrowserRouter>
-    );
+    const { container } = render(<Selection />);
     expect(container).toMatchSnapshot();
   });
 });
