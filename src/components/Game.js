e0222cfc1d1b4dc791d2a8d55da94be18e4b08b9
RhazzXIX <jm21tonio@gmail.com>
2023-04-19 13:46:45 +0800

    Refactor Game component into a dynamic page
    Made Game component into a dynamic page for loading needed context only,
    depending on the url dynamic segment.
    Tailored Game component html structure and style accordingly.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,54 +1,44 @@
-import { useState, useEffect } from "react";
-import { database, storage } from "../assists/fbConfig";
-import { getDownloadURL, ref } from "firebase/storage";
-import { getDoc, doc, collection } from "firebase/firestore";
-import Selection from "./Selection";
-import useGameData from "../assists/useGameData";
 import "../styles/Game.css";
+import useGameData from "../assists/useGameData";
+import { useParams } from "react-router-dom";
 
 export default function Game(props) {
-  const [showFirstMap, setShowFirstMap] = useState(true);
-  const [showScndMap, setShowScndmap] = useState(false);
+  const { gameVer } = useParams();
+
   const { firstMap, scndMap, charSelection1, charSelection2 } = useGameData();
+
+  let map, charSelection;
+
+  switch (gameVer) {
+    case "animeX":
+      map = firstMap;
+      charSelection = charSelection1;
+      break;
+    case "gameX":
+      map = scndMap;
+      charSelection = charSelection2;
+      break;
+    default:
+      map = null;
+      charSelection = null;
+  }
 
   return (
     <main id="game">
-      {showFirstMap && (
-        <section className="map">
-          <header>
-            <ul>
-              {charSelection1.map((char) => {
-                return (
-                  <li key={char.name}>
-                    <img src={char.url} alt={char.name} className="chars" />
-                    <p>{char.name}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </header>
-          <img src={firstMap.url} alt={firstMap.name} />
-        </section>
-      )}
-      {showScndMap && (
-        <section className="map">
-          <header>
-            <ul>
-              {charSelection2.map((char) => {
-                return (
-                  <li key={char.name}>
-                    <img src={char.url} alt={char.name} className="chars" />
-                    <p>{char.name}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </header>
-          <img src={scndMap.url} alt={scndMap.name} />
-        </section>
-      )}
-
-      {/* <Selection /> */}
+      <header>
+        <ul>
+          {charSelection &&
+            charSelection.map((char) => {
+              return (
+                <li key={char.name}>
+                  <img src={char.url} alt={char.name} className="chars" />
+                  <p>{char.name}</p>
+                </li>
+              );
+            })}
+        </ul>
+      </header>
+      {map && <img src={map.url} alt={map.name} />}
     </main>
   );
 }
