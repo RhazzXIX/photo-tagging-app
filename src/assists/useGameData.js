04e9d83920bee6cf41d32871694e52f30abf63a2
RhazzXIX <jm21tonio@gmail.com>
2023-04-24 16:45:01 +0800

    Refactor to use assists factory map & char fns

--- src/assists/useGameData.js
+++ src/assists/useGameData.js
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import { database, storage } from "./fbConfig";
 import { getDownloadURL, ref } from "firebase/storage";
 import { getDoc, doc } from "firebase/firestore";
+import createMapObj from "../assists/createMapObj";
+import createCharObj from "./createCharObj";
 
 export default function useGameData(props) {
-  const [firstMap, setFirstmap] = useState({});
+  const [firstMap, setFirstMap] = useState({});
   const [scndMap, setScndMap] = useState({});
   const [charSelection1, setCharSelection1] = useState([]);
   const [charSelection2, setCharSelection2] = useState([]);
@@ -13,64 +15,58 @@ export default function useGameData(props) {
   const selection2Ref = doc(database, "game", "game");
 
   const getData = async () => {
-    const firstMapData = {};
-    const scndMapData = {};
-    const char1 = {};
-    const char2 = {};
-    const char3 = {};
-    const char4 = {};
-    const char5 = {};
-    const char6 = {};
+    const charSelection1Data = [];
+    const charSelection2DAta = [];
 
     const data1 = await getDoc(selection1Ref).then((response) =>
       response.data()
     );
-    firstMapData.name = data1.map.name;
-    await getDownloadURL(ref(storage, data1.map.imgRef)).then(
-      (url) => (firstMapData.url = url)
-    );
-    await getDownloadURL(ref(storage, data1.map.miniImgRef)).then((url) => {
-      firstMapData.miniUrl = url;
+    await getDownloadURL(ref(storage, data1.map.imgRef)).then(async (url) => {
+      await getDownloadURL(ref(storage, data1.map.miniImgRef)).then(
+        (miniUrl) => {
+          setFirstMap(createMapObj(data1.map.name, url, miniUrl));
+        }
+      );
     });
-    setFirstmap(firstMapData);
-    char1.name = data1.char1.name;
-    await getDownloadURL(ref(storage, data1.char1.imgRef)).then(
-      (url) => (char1.url = url)
-    );
-    char2.name = data1.char2.name;
-    await getDownloadURL(ref(storage, data1.char2.imgRef)).then(
-      (url) => (char2.url = url)
-    );
-    char3.name = data1.char3.name;
-    await getDownloadURL(ref(storage, data1.char3.imgRef)).then(
-      (url) => (char3.url = url)
-    );
-    setCharSelection1([char1, char2, char3]);
+    await getDownloadURL(ref(storage, data1.char1.imgRef)).then((url) => {
+      const char = createCharObj(data1.char1.name, url, selection1Ref);
+      charSelection1Data.push(char);
+    });
+    await getDownloadURL(ref(storage, data1.char2.imgRef)).then((url) => {
+      const char = createCharObj(data1.char2.name, url, selection1Ref);
+      charSelection1Data.push(char);
+    });
+    await getDownloadURL(ref(storage, data1.char3.imgRef)).then((url) => {
+      const char = createCharObj(data1.char3.name, url, selection1Ref);
+      charSelection1Data.push(char);
+    });
+    setCharSelection1(charSelection1Data);
 
     const data2 = await getDoc(selection2Ref).then((response) =>
       response.data()
     );
-    scndMapData.name = data2.map.name;
-    await getDownloadURL(ref(storage, data2.map.imgRef)).then(
-      (url) => (scndMapData.url = url)
-    );
-    await getDownloadURL(ref(storage, data2.map.miniImgRef)).then((url) => {
-      scndMapData.miniUrl = url;
+
+    await getDownloadURL(ref(storage, data2.map.imgRef)).then(async (url) => {
+      await getDownloadURL(ref(storage, data2.map.miniImgRef)).then(
+        (miniUrl) => {
+          setScndMap(createMapObj(data2.map.name, url, miniUrl));
+        }
+      );
     });
-    setScndMap(scndMapData);
-    char4.name = data2.char1.name;
-    await getDownloadURL(ref(storage, data2.char1.imgRef)).then(
-      (url) => (char4.url = url)
-    );
-    char5.name = data2.char2.name;
-    await getDownloadURL(ref(storage, data2.char2.imgRef)).then(
-      (url) => (char5.url = url)
-    );
-    char6.name = data2.char3.name;
-    await getDownloadURL(ref(storage, data2.char3.imgRef)).then(
-      (url) => (char6.url = url)
-    );
-    setCharSelection2([char4, char5, char6]);
+
+    await getDownloadURL(ref(storage, data2.char1.imgRef)).then((url) => {
+      const char = createCharObj(data2.char1.name, url, selection2Ref);
+      charSelection2DAta.push(char);
+    });
+    await getDownloadURL(ref(storage, data2.char2.imgRef)).then((url) => {
+      const char = createCharObj(data2.char2.name, url, selection2Ref);
+      charSelection2DAta.push(char);
+    });
+    await getDownloadURL(ref(storage, data2.char3.imgRef)).then((url) => {
+      const char = createCharObj(data2.char3.name, url, selection2Ref);
+      charSelection2DAta.push(char);
+    });
+    setCharSelection2(charSelection2DAta);
   };
 
   useEffect(() => {
