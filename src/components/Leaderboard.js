bf65faa9c6764e332bd83afa0a20c80d19eb9bbb
RhazzXIX <jm21tonio@gmail.com>
2023-04-28 01:33:21 +0800

    Edit html structure & Add logic on fetching images

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,20 +1,44 @@
-export default function Leaderboard() {
+import parseToTimer from "../assists/parseToTimer";
+import useGameData from "../assists/useGameData";
+import { useState } from "react";
+
+export default function Leaderboard({ gameVer }) {
+  const [game, setGame] = useState("Anime Crossover");
+  const [recordList, setRecordList] = useState([]);
+
+  const { firstMap, scndMap } = useGameData();
+
+  const timeToString = (time) => {
+    const { minutes, seconds, milliseconds } = parseToTimer(time);
+
+    return `${minutes}m ${seconds}s ${milliseconds}ms`;
+  };
+
   return (
-    <main>
-      <button>Back to game</button>
-      <button></button>
-      <button></button>
-      <section>
-        <header>
-          <p>No.</p>
-          <p>Name</p>
-          <p>Time</p>
-        </header>
-        <ul>
-          <li>
-            <div></div>
-          </li>
-        </ul>
+    <main id="LeaderBoard">
+      <header>
+        <button>
+          {firstMap && <img src={firstMap.miniUrl} alt={firstMap.name} />}
+        </button>
+        <button>
+          {scndMap && <img src={scndMap.miniUrl} alt={scndMap.name} />}
+        </button>
+      </header>
+      <h1>{game}</h1>
+      <section className="list">
+        <h4>Name:</h4>
+        <h4>Time:</h4>
+        <ol>
+          {recordList.map((record, i) => {
+            const key = Object.keys(recordList);
+            return (
+              <li key={key[i]}>
+                <p>{record.userName}</p>
+                <p>{timeToString(record.time)}</p>
+              </li>
+            );
+          })}
+        </ol>
       </section>
     </main>
   );
--- src/tests/Leaderboard.test.js
+++ src/tests/Leaderboard.test.js
@@ -2,10 +2,57 @@ import "@testing-library/jest-dom";
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Leaderboard from "../components/Leaderboard";
+import useGameData from "../assists/useGameData";
+import { doc, getDoc } from "firebase/firestore";
+
+const fetchedMapData = {
+  firstMap: {
+    miniUrl: "mini url 1",
+    name: "Anime Crossover",
+    url: "url 2",
+  },
+  scndMap: {
+    miniUrl: "mini url 2",
+    name: "Game Crossover",
+    url: "url 2",
+  },
+};
+
+const fetchedLeaderBoardData = {
+  record0: {
+    userName: "test1",
+    time: 5500,
+  },
+  record1: {
+    userName: "test1",
+    time: 5500,
+  },
+};
+
+jest.mock("../assists/useGameData");
+jest.mock("firebase/firestore");
+
+doc.mockReturnValue("testRef");
+getDoc.mockResolvedValue({
+  data() {
+    return fetchedLeaderBoardData;
+  },
+});
+
+jest.mock("../assists/useGameData", () => {
+  return () => fetchedMapData;
+});
 
 describe("Leaderboard component", () => {
   it("Renders on screen", () => {
     const { container } = render(<Leaderboard />);
     expect(container).toMatchSnapshot();
+  });
+
+  it("Renders the fetched images", () => {
+    render(<Leaderboard />);
+
+    expect(screen.getByAltText("Anime Crossover")).toBeInTheDocument();
+    expect(screen.getByAltText("Game Crossover")).toBeInTheDocument();
   });
 });
