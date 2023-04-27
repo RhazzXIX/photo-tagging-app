31b8de95c58c0159ae95f1d9287166cff0c81943
RhazzXIX <jm21tonio@gmail.com>
2023-04-27 17:10:29 +0800

    Add logic to redirect page to the leader boards

--- src/components/RecordForm.js
+++ src/components/RecordForm.js
@@ -1,34 +1,42 @@
+import "../styles/RecordForm.css";
 import parseToTimer from "../assists/parseToTimer";
 import { setDoc, doc, updateDoc, getDoc } from "firebase/firestore";
 import { database } from "../assists/fbConfig";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function RecordForm(props) {
   const { time, gameVer } = props;
   const [userName, setUserName] = useState("");
+  const [recordSent, setRecordSent] = useState(false);
+  const navigate = useNavigate();
 
   async function sendDocs(e) {
     e.preventDefault();
-    let totalRecord;
-    const docRef = doc(database, "leaderBoard", gameVer);
-    const leaderBoard = await getDoc(docRef).then((response) =>
-      response.data()
-    );
+    if (!recordSent) {
+      let totalRecord;
+      const docRef = doc(database, "leaderBoard", gameVer);
+      const leaderBoard = await getDoc(docRef).then((response) =>
+        response.data()
+      );
 
-    if (!leaderBoard) {
-      totalRecord = 0;
-    } else {
-      totalRecord = Object.keys(leaderBoard).length;
-    }
+      if (!leaderBoard) {
+        totalRecord = 0;
+      } else {
+        totalRecord = Object.keys(leaderBoard).length;
+      }
 
-    const playerRecord = {
-      [`record${totalRecord}`]: { userName, time },
-    };
+      const playerRecord = {
+        [`record${totalRecord}`]: { userName, time },
+      };
 
-    if (!leaderBoard) {
-      await setDoc(docRef, playerRecord);
-    } else {
-      await updateDoc(docRef, playerRecord);
+      if (!leaderBoard) {
+        await setDoc(docRef, playerRecord);
+      } else {
+        await updateDoc(docRef, playerRecord);
+      }
+      setRecordSent(true);
+      navigate("/photo-tagging-app/leaderboard");
     }
   }
 
@@ -38,26 +46,28 @@ export default function RecordForm(props) {
 
   const { minutes, seconds, milliseconds } = parseToTimer(time);
   return (
-    <form id="record" onSubmit={sendDocs}>
-      <div className="timer">
-        <p>Your time is:</p>
-        <h4>
-          {minutes}m {seconds}s {milliseconds}ms
-        </h4>
-      </div>
-      <label htmlFor="name">
-        Name:
-        <input
-          type="text"
-          id="name"
-          value={userName}
-          onChange={handleInputName}
-          required
-        />
-      </label>
-      <button type="submit" className="submit">
-        Submit
-      </button>
-    </form>
+    <section className="form">
+      <form id="record" onSubmit={sendDocs}>
+        <div className="timer">
+          <p>Your time is:</p>
+          <h4>
+            {minutes}m {seconds}s {milliseconds}ms
+          </h4>
+        </div>
+        <label htmlFor="name">
+          Name:
+          <input
+            type="text"
+            id="name"
+            value={userName}
+            onChange={handleInputName}
+            required
+          />
+        </label>
+        <button type="submit" className="submit">
+          Submit
+        </button>
+      </form>
+    </section>
   );
 }
--- src/tests/RecordForm.test.js
+++ src/tests/RecordForm.test.js
@@ -3,6 +3,11 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import RecordForm from "../components/RecordForm";
 import { setDoc, doc, getDoc } from "firebase/firestore";
+import {
+  BrowserRouter,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
 
 jest.mock("firebase/firestore", () => {
   const originalModule = jest.requireActual("firebase/firestore");
@@ -21,21 +26,37 @@ const docRef = "testRef";
 
 describe("RecordForm component", () => {
   it("Renders on screen", () => {
-    const { container } = render(<RecordForm />);
+    const { container } = render(
+      <BrowserRouter>
+        <RecordForm />
+      </BrowserRouter>
+    );
 
     expect(container).toMatchSnapshot();
   });
   it("Shows the user the time it took playing the game", () => {
-    const { rerender } = render(<RecordForm time={5500} />);
+    const { rerender } = render(
+      <BrowserRouter>
+        <RecordForm time={5500} />
+      </BrowserRouter>
+    );
 
     expect(screen.getByRole("heading").textContent).toBe("0m 5s 500ms");
 
-    rerender(<RecordForm time={60456} />);
+    rerender(
+      <BrowserRouter>
+        <RecordForm time={60456} />
+      </BrowserRouter>
+    );
 
     expect(screen.getByRole("heading").textContent).toBe("1m 0s 456ms");
   });
   it("Lets the user input his name in the name input", async () => {
-    render(<RecordForm time={5500} />);
+    render(
+      <BrowserRouter>
+        <RecordForm time={5500} />
+      </BrowserRouter>
+    );
 
     const nameInput = screen.getByLabelText("Name:");
 
@@ -44,33 +65,104 @@ describe("RecordForm component", () => {
     expect(nameInput.value).toBe("Juan");
   });
 
-  it("Lets users submit their record", async () => {
-    doc.mockReturnValue(docRef);
-    getDoc.mockResolvedValue({
-      data() {},
+  describe("Sending record", () => {
+    beforeEach(() => {
+      doc.mockReturnValue(docRef);
+      getDoc.mockResolvedValue({
+        data() {},
+      });
     });
-    render(<RecordForm time={5500} gameVer={"animeX"} />);
 
-    const nameInput = screen.getByLabelText("Name:");
-    const submitBtn = screen.getByRole("button", { name: /submit/i });
+    it("Lets users submit their record", async () => {
+      render(
+        <BrowserRouter>
+          <RecordForm time={5500} gameVer={"animeX"} />
+        </BrowserRouter>
+      );
 
-    await user.type(nameInput, "Juan");
+      const nameInput = screen.getByLabelText("Name:");
+      const submitBtn = screen.getByRole("button", { name: /submit/i });
 
-    await user.click(submitBtn);
+      await user.type(nameInput, "Juan");
 
-    expect(doc).toHaveBeenCalled();
-    expect(doc.mock.calls[0][1]).toBe("leaderBoard");
-    expect(doc.mock.calls[0][2]).toBe("animeX");
+      await user.click(submitBtn);
 
-    expect(getDoc.mock.calls[0][0]).toBe("testRef");
+      expect(doc).toHaveBeenCalled();
+      expect(doc.mock.calls[0][1]).toBe("leaderBoard");
+      expect(doc.mock.calls[0][2]).toBe("animeX");
 
-    expect(setDoc).toHaveBeenCalled();
-    expect(setDoc.mock.calls[0][0]).toBe("testRef");
-    expect(setDoc.mock.calls[0][1]).toEqual({
-      record0: {
-        userName: "Juan",
-        time: 5500,
-      },
+      expect(getDoc).toHaveBeenCalled();
+      expect(getDoc.mock.calls[0][0]).toBe("testRef");
+
+      expect(setDoc).toHaveBeenCalled();
+      expect(setDoc.mock.calls[0][0]).toBe("testRef");
+      expect(setDoc.mock.calls[0][1]).toEqual({
+        record0: {
+          userName: "Juan",
+          time: 5500,
+        },
+      });
+    });
+
+    it("Only lets the user submit their record once", async () => {
+      render(
+        <BrowserRouter>
+          <RecordForm time={5500} gameVer={"animeX"} />
+        </BrowserRouter>
+      );
+
+      const nameInput = screen.getByLabelText("Name:");
+      const submitBtn = screen.getByRole("button", { name: /submit/i });
+
+      await user.type(nameInput, "Juan");
+
+      await user.click(submitBtn);
+
+      expect(doc).toHaveBeenCalled();
+      expect(getDoc).toHaveBeenCalled();
+      expect(setDoc).toHaveBeenCalled();
+
+      await user.click(submitBtn);
+
+      expect(doc).toBeCalledTimes(1);
+      expect(getDoc).toBeCalledTimes(1);
+      expect(setDoc).toBeCalledTimes(1);
+    });
+
+    it("Loads the leaderboard page when the submit button is pressed", async () => {
+      const routes = [
+        {
+          path: "/photo-tagging-app",
+          element: <RecordForm time={5500} gameVer={"animeX"} />,
+        },
+        {
+          path: "/photo-tagging-app/leaderboard",
+          element: <h2>Leader Board</h2>,
+        },
+      ];
+
+      const router = createMemoryRouter(routes, {
+        initialEntries: [
+          "/photo-tagging-app",
+          "/photo-tagging-app/leaderboard",
+        ],
+        initialIndex: 0,
+      });
+
+      render(<RouterProvider router={router} />);
+
+      const nameInput = screen.getByLabelText("Name:");
+      const submitBtn = screen.getByRole("button", { name: /submit/i });
+
+      await user.type(nameInput, "Juan");
+
+      await user.click(submitBtn);
+
+      expect(
+        screen.getByRole("heading", { name: /leader board/i })
+      ).toBeInTheDocument();
+      expect(nameInput).not.toBeInTheDocument();
+      expect(submitBtn).not.toBeInTheDocument();
     });
   });
 });
