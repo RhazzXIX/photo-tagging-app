f666f3a4ee6794b57956bcf73e984bc548a109f9
RhazzXIX <jm21tonio@gmail.com>
2023-04-29 15:55:31 +0800

    Refactor routes
    Added all component routes that will be used in the page.
    Added all component routes for the kids version.

--- src/App.js
+++ src/App.js
@@ -2,32 +2,60 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import FrontPage from "./components/FrontPage";
 import Home from "./components/Home";
 import Game from "./components/Game";
+import Selection from "./components/Selection";
+import Leaderboard from "./components/Leaderboard";
 
-const router = createBrowserRouter([
+const appRoutes = [
   {
-    path: "photo-tagging-app",
-    element: <Home />,
+    path: "/photo-tagging-app",
+    element: <Home key={"normal"} />,
     children: [
       {
         path: "",
         element: <FrontPage key={"normal"} />,
       },
       {
-        path: "kids",
+        path: "game",
+        element: <Selection key={"normal"} />,
+      },
+      {
+        path: "leaderboard",
+        element: <Leaderboard key={"normal"} />,
+      },
+      {
+        path: "game/:gameVer",
+        element: <Game key={"normal"} />,
+      },
+    ],
+  },
+  {
+    path: "/photo-tagging-app/kids",
+    element: <Home key={"kids"} version={"kids"} />,
+    children: [
+      {
+        path: "",
         element: <FrontPage key={"kids"} version={"kids"} />,
       },
       {
         path: "game",
-        element: <Game key={"normal"} />,
+        element: <Selection key={"kids"} version={"kids"} />,
       },
       {
-        path: "kids/game",
+        path: "game/:gameVer",
         element: <Game key={"kids"} version={"kids"} />,
+      },
+      {
+        path: "leaderboard",
+        element: <Leaderboard key={"kids}"} version={"kids"} />,
       },
     ],
   },
-]);
+];
+
+const router = createBrowserRouter(appRoutes);
 
 const App = <RouterProvider router={router} />;
 
 export default App;
+
+export { appRoutes };
