81c845b68c11d5c700019f1132439a6c0dc73d6e
RhazzXIX <jm21tonio@gmail.com>
2023-05-02 15:51:30 +0800

    Edit header structure & add logic to suit kids ver.
    Changed Credits to Leaderboard link.
    Added logic to change home link ref that points to kids version.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,22 @@
-import { Link, Outlet } from "react-router-dom"
-import ghLogo from '../assets/gitW.png'
-import '../styles/Home.css'
+import { Link, Outlet } from "react-router-dom";
+import ghLogo from "../assets/gitW.png";
+import "../styles/Home.css";
 
-export default function Home (props) {
-  return(
+export default function Home({ version }) {
+  let homeUrlRef = "/photo-tagging-app";
+  if (version === "kids") homeUrlRef = "/photo-tagging-app/kids";
+  return (
     <>
-    <header>
-      <Link to="/" >
-        Home
-      </Link>
-      <Link to='/credits'>
-        Credits
-      </Link>
-    </header>
-    <Outlet />
-    <footer>
-      <a href="https://github.com/RhazzXIX">
-        <img src={ghLogo} alt="GitHub" />
-      </a>
-    </footer>
+      <header>
+        <Link to={homeUrlRef}>Home</Link>
+        <Link to="leaderboard">Leaderboard</Link>
+      </header>
+      <Outlet />
+      <footer>
+        <a href="https://github.com/RhazzXIX">
+          <img src={ghLogo} alt="GitHub" />
+        </a>
+      </footer>
     </>
-  )
+  );
 }
