9897844cd6869ac0e53a7d910ff40b2e8d026280
RhazzXIX <jm21tonio@gmail.com>
2023-05-02 16:03:18 +0800

    Fix typo

--- src/tests/parseTimer.test.js
+++ src/tests/parseTimer.test.js
@@ -5,27 +5,27 @@ describe("parseTimer helper function", () => {
     expect(parseToTimer(100)).toEqual({
       minutes: 0,
       seconds: 0,
-      milliSeconds: 100,
+      milliseconds: 100,
     });
     expect(parseToTimer(1000)).toEqual({
       minutes: 0,
       seconds: 1,
-      milliSeconds: 0,
+      milliseconds: 0,
     });
     expect(parseToTimer(60000)).toEqual({
       minutes: 1,
       seconds: 0,
-      milliSeconds: 0,
+      milliseconds: 0,
     });
     expect(parseToTimer(60001)).toEqual({
       minutes: 1,
       seconds: 0,
-      milliSeconds: 1,
+      milliseconds: 1,
     });
     expect(parseToTimer(61100)).toEqual({
       minutes: 1,
       seconds: 1,
-      milliSeconds: 100,
+      milliseconds: 100,
     });
   });
 });
